7390a6dbaf3198eeedc4b0822d969d8602005e17
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-08-27 21:44:51 -0700

    add in tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
+import tailwind from '@astrojs/tailwind';
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://spokanejohanne.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
   adapter: netlify(),
 });
